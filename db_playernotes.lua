ceb72c0a3799b260344ced6a24134eb6f43f37d1
marksn0w <85539266+marksn0w@users.noreply.github.com>
2021-06-12 23:47:23 -0400

    Database update

--- db_playernotes.lua
+++ db_playernotes.lua
@@ -4,5 +4,7 @@ playerdata = {
     {"Selkie-Ragnaros", "AFK in mythic while spamming nonsense"},
     {"Griggy-Drak'thul", "joined mythic and left at final boss"},
     {"Feldemonizer-Arthas", "throwing dungeon and being racist"},
+    {"Feldemonizer-Arthas", "Excessive toxicity"},
+    {"Elysiá-Sargeras", "Excessive toxicity for no good reason"},
     {"Blàzer-Stormrage", "Making racist comments towards a Chinese player"}
 }
